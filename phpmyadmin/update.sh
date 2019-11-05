407e8fce9410ec8cd2614d749836fa17553cb5f3
devopsUser <rrofail@hotmail.com>
2019-11-05 17:57:07 -0500

    Phpmyadmin setup to connect to mysql demo

---
+++ phpmyadmin/update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# Check for dependencies
+command -v curl >/dev/null 2>&1 || { echo >&2 "'curl' is required but not found. Aborting."; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "'jq' is required but not found. Aborting."; exit 1; }
+if [ -z "${BASH_VERSINFO}" ] || [ -z "${BASH_VERSINFO[0]}" ] || [ ${BASH_VERSINFO[0]} -lt 4 ]; then
+  echo "BASH version 4.0 or greater is required. Aborting."
+  exit 1
+fi
+
+declare -A cmd=(
+	[apache]='apache2-foreground'
+	[fpm]='php-fpm'
+	[fpm-alpine]='php-fpm'
+)
+
+declare -A base=(
+	[apache]='debian'
+	[fpm]='debian'
+	[fpm-alpine]='alpine'
+)
+
+latest="$(curl -fsSL 'https://www.phpmyadmin.net/home_page/version.json' | jq -r '.version')"
+
+for variant in apache fpm fpm-alpine; do
+	template="Dockerfile-${base[$variant]}.template"
+	cp $template "$variant/Dockerfile"
+	cp config.inc.php "$variant/config.inc.php"
+	cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
+	sed -ri -e '
+		s/%%VERSION%%/'"$latest"'/;
+		s/%%VARIANT%%/'"$variant"'/;
+		s/%%CMD%%/'"${cmd[$variant]}"'/;
+	' "$variant/Dockerfile"
+done
